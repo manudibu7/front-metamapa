b4d33ee831d693653aad8dd6f43ca8ade58aaacd
fabricioalg <flopezgarro@frba.utn.edu.ar>
2026-02-21 00:41:22 -0300

    esto no sirve

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renderiza el hero principal', async () => {
-  render(<App />);
-  expect(await screen.findByText(/Información geolocalizada/i)).toBeInTheDocument();
-});
